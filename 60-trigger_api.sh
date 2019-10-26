dfbd8f62d8c868ae61eea11837d41c20867f4812
Stuart Green <stuart@root9.net>
2019-10-26 15:13:31 +0100

    Update 60-trigger_api.sh
    changed directory for script

--- 60-trigger_api.sh
+++ 60-trigger_api.sh
@@ -3,7 +3,7 @@
 if ([ $reason = "BOUND" ] || [ $reason = "RENEW" ] || [ $reason = "CARRIER"])
 then
         ipsec stop
-        /usr/bin/python3 /home/pi/ip_update.py 2>&1 >> /var/log/ip_update.log
+        /usr/bin/python3 /home/pi/NSaaS_Pi/ip_update.py 2>&1 >> /var/log/ip_update.log
         echo "interface up script triggered" >> /var/log/ip_update.log
         ipsec start
 fi
